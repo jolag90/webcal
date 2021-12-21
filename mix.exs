19b89f914814d916a56b96c10f72508cd59b3151
Jonasgit@github.com:jolag90/ex_practicing.git <jonas@iboard.cc>
2021-12-21 15:35:14 +0100

    Clean up scaffolded html

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Webcal.MixProject do
     [
       app: :webcal,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
